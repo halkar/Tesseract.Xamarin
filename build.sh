b1cbb22d0a884618d978cb9b34069a3ab1c80100
Artur Shamsutdinov <artur.shamsutdinov@gmail.com>
2015-08-02 14:40:07 +1000

    Added nuget packages restore to build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 
 git submodule init
 
+mono --runtime=v4.0 nuget/NuGet.exe restore Tesseract.Xamarin.sln
+
 xbuild /p:Configuration=Release Tesseract.Xamarin.sln
 
 mono --runtime=v4.0 nuget/NuGet.exe pack Xamarin.Tesseract.nuspec
