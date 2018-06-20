024dd21ebf8398f4e686eb75fdb6ac282e09305e
William Barbosa <william.sb@hotmail.com>
2018-06-20 16:35:23 -0300

    Restore original AssemblyInfo behaviour

---
+++ Tesseract/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+// Information about this assembly is defined by the following attributes.
+// Change them to the values specific to your project.
+
+[assembly: AssemblyTitle ("Tesseract")]
+[assembly: AssemblyDescription ("")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("")]
+[assembly: AssemblyProduct ("")]
+[assembly: AssemblyCopyright ("artur")]
+[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyCulture ("")]
